1ecf14fbd758db28a05acbc8fb7fbe6e1daa084f
Mary Nazarova <MGKrupkina@yandex.ru>
2024-02-20 14:20:21 +0300

    project mesto

--- src/index.js
+++ src/index.js
@@ -12,11 +12,27 @@ import {
   closeModal,
 } from "./scripts/modal.js";
 
+import { enableValidation, clearValidation } from './scripts/validation.js';
+//import { getCardsFromServer, getProfileInfoFromServer, updateProfileInfoOnServer,
+ // addCardToServer, updatAvatarOnServer } from './scripts/api.js';
+
+const validationConfig = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible",
+};
+
+enableValidation(validationConfig);
+
 // Находим форму в DOM
 // Воспользуйтесь методом querySelector()
 
 const formEditProfile = document.forms['edit-profile'];
 const formCard = document.forms['new-place'];
+
 
 // Находим поля формы в DOM
 const nameInput = document.querySelector('.popup__input_type_name');
@@ -31,6 +47,8 @@ const buttonAdd = document.querySelector('.profile__add-button');
 const newCard = document.querySelector('.popup_type_new-card');
 const cardsContainer = document.querySelector('.places__list');
 const popupType = document.querySelector('.popup_type_image');
+const profileEditForm = document.querySelector('.popup__form');
+
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -54,6 +72,7 @@ function handleFormSubmit(event) {
 
 formEditProfile.addEventListener('submit', handleFormSubmit);
 
+
 // Photo content 
 
 initialCards.forEach((card) => {
@@ -76,11 +95,10 @@ function handleCardFormSubmit(event) {
       link: urlInput.value,
     }
     cardsContainer.prepend(createCard(card, deletePhoto, likeIcon, mestoImage));
-    
     formCard.reset();
-
     closeModal(newCard);
 }
+
 formCard.addEventListener('submit', handleCardFormSubmit);
 
 // Edit profile 
@@ -88,12 +106,18 @@ formCard.addEventListener('submit', handleCardFormSubmit);
 buttonProfile.addEventListener('click', function () {
   jobInput.value = document.querySelector('.profile__description').textContent;
   nameInput.value = document.querySelector('.profile__title').textContent;
+  clearValidation(profileEditForm, validationConfig);
   openModal(userProfile);
 });
 
-// Form fot plus button
+
+// Form for plus button
 
 buttonAdd.addEventListener('click', function () {
+  clearValidation(formCard, validationConfig);
   openModal(newCard);
 });
+
+
+//Form edit avatar
 
---
+++ src/scripts/validation.js
@@ -0,0 +1,122 @@
+//validation.js
+//когда условие в операторе if истина
+
+//validation.js
+
+function isNameValid(name) {
+  return /^[A-Za-zА-Яа-яЁё\s-]*$/.test(name);
+}
+
+function isDescriptionValid(description) {
+  return /^[A-Za-zА-Яа-яЁё\s-]*$/.test(description);
+}
+
+function setCustomValidity(onInput) {
+  if (onInput.validity.valueMissing) { //проверяем, имеет ли элемент управления required
+    onInput.setCustomValidity("Вы пропустили это поле.");
+  } else if (
+    (onInput.name === "name" || onInput.name === "place-name") &&
+    !isNameValid(onInput.value)
+  ) {
+    onInput.setCustomValidity(
+      "Разрешены только латинские, кириллические буквы, знаки дефиса и пробелы."
+    );
+  } else if (
+    (onInput.name === "description" ||
+    onInput.name === "place-name") &&
+    !isDescriptionValid(onInput.value)
+  ) {
+    onInput.setCustomValidity(
+      "Разрешены только латинские, кириллические буквы, знаки дефиса и пробелы."
+    );
+  } else if (
+    onInput.name === "place-name" &&
+    onInput.validity.tooShort //проверяем длину значения, которую ввел пользователь, если она меньше минимально допустимого значения, 
+    //заданного в атрибуте minlength
+  ) {
+    onInput.setCustomValidity("Минимальная длина - 2 символа.");
+  } else if (
+    onInput.name === "link" &&
+    onInput.validity.patternMismatch // проверяем соответствует ли значение правилу, которое указано в pattern
+  ) {
+    onInput.setCustomValidity("Введите адрес сайта.");
+  } else {
+    onInput.setCustomValidity("");
+  }
+}
+
+function checkInputValidity(onInput, settings) {
+  setCustomValidity(onInput);
+  if (!onInput.validity.valid) {
+    checkError(onInput, onInput.validationMessage, settings);
+  } else {
+    hideInputError(onInput, settings);
+  }
+}
+
+function checkError(onInput, errorMessage, settings) {
+  const errorElement = onInput.nextElementSibling;
+  onInput.classList.add(settings.inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(settings.errorClass);
+}
+
+function hideInputError(onInput, settings) {
+  const errorElement = onInput.nextElementSibling;
+  onInput.classList.remove(settings.inputErrorClass);
+  errorElement.classList.remove(settings.errorClass);
+  errorElement.textContent = "";
+}
+
+function toggleButtonState(inputs, buttonElement, settings) {
+  const isFormValid = inputs.every((input) => input.validity.valid);
+  buttonElement.disabled = !isFormValid;
+  buttonElement.classList.toggle(settings.inactiveButtonClass, !isFormValid);
+}
+
+export function clearValidation(formElement, settings) {
+  const inputs = Array.from(
+    formElement.querySelectorAll(settings.inputSelector)
+  );
+  inputs.forEach((onInput) => {
+    hideInputError(onInput, settings);
+  });
+
+  const submitButton = formElement.querySelector(settings.submitButtonSelector);
+  toggleButtonState(inputs, submitButton, settings);
+}
+
+export function enableValidation(settings) {
+  const forms = document.querySelectorAll(settings.formSelector);
+  forms.forEach((form) => {
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+    });
+
+    const inputs = Array.from(form.querySelectorAll(settings.inputSelector));
+    inputs.forEach((onInput) => {
+      onInput.addEventListener("input", () => {
+        checkInputValidity(onInput, settings);
+        toggleButtonState(
+          inputs,
+          form.querySelector(settings.submitButtonSelector),
+          settings
+        );
+      });
+
+      onInput.addEventListener("focus", () => {
+        hideInputError(onInput, settings);
+      });
+
+      onInput.addEventListener("blur", () => {
+        checkInputValidity(onInput, settings);
+      });
+    });
+
+    toggleButtonState(
+      inputs,
+      form.querySelector(settings.submitButtonSelector),
+      settings
+    );
+  });
+}
